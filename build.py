c6fe0537f5db0a363146e84b78c7aa57938230ee
Zack Zhou <zackzhh@gmail.com>
2012-02-22 17:24:03 +0800

    Add .rc file included version info, icon, sample file

--- build.py
+++ build.py
@@ -2,23 +2,18 @@ import os
 import re
 
 if __name__ == '__main__':
-	# JumplistZ.sample.ini --> JumplistZ.sample.h
-	with open('JumplistZ.sample.ini') as f:
-		with open('JumplistZ.sample.h', 'w') as o:
-			o.write('\nLPCSTR szSampleINI =\n');
-			for line in f:
-				str = re.sub(r'(\\|")', r'\\\g<1>', line.strip())
-				newline = '\t"%s\\n"\n' % str.ljust(65, ' ')
-				o.write(newline);
-			o.write('\t"";\n');
-
 	# Build JumplistZ.exe
-	cpp = 'JumplistZ.cpp'
-	with open(cpp) as f:
+	with open('JumplistZ.cpp') as f:
 		txt = f.read()
 		name = re.findall(r'\s+NAME\[\]\s*=\s*_T\("(.*?)"', txt)
 		ver = re.findall(r'\s+VERSION\[\]\s*=\s*_T\("(.*?)"', txt)
 		fnout = '%s-%s.exe' % (name[0], ver[0])
 	sdkenv = r'C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.Cmd'
-	os.system(r'call "%s" /release /x86 /xp && cl %s /Fe%s' % (sdkenv, cpp, fnout));
-
+	cmds = [
+		'call "%s" /release /x86 /xp' % sdkenv,
+		'rc JumplistZ.rc',
+		'cl JumplistZ.cpp',
+		'link JumplistZ.obj JumplistZ.res /out:%s' % fnout,
+		]
+	ret = os.system(' && '.join(cmds))
+	print 'ret: %d' % ret
