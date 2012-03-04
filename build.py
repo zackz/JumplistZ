1b4bfd92ff3dbadf87b70bf319654314e4499a96
Zack Zhou <zackzhh@gmail.com>
2012-03-04 14:03:14 +0800

    Update comments

--- build.py
+++ build.py
@@ -2,12 +2,13 @@ import os
 import re
 
 if __name__ == '__main__':
-	# Build JumplistZ.exe
+	# Get new name
 	with open('JumplistZ.cpp') as f:
 		txt = f.read()
-		name = re.findall(r'\s+NAME\[\]\s*=\s*_T\("(.*?)"', txt)
-		ver = re.findall(r'\s+VERSION\[\]\s*=\s*_T\("(.*?)"', txt)
-		fnout = '%s-%s.exe' % (name[0], ver[0])
+	name = re.findall(r'\s+NAME\[\]\s*=\s*_T\("(.*?)"', txt)
+	ver = re.findall(r'\s+VERSION\[\]\s*=\s*_T\("(.*?)"', txt)
+	fnout = '%s-%s.exe' % (name[0], ver[0])
+	# Build JumplistZ.exe
 	sdkenv = r'C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.Cmd'
 	cmds = [
 		'call "%s" /release /x86 /xp' % sdkenv,
