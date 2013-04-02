335ab3a196501b1b54239c56b5a03cacbbcc309e
Zack Zhou <zackzhh@gmail.com>
2013-04-02 10:05:18 +0800

    Put executable file to distribution directory.

--- build.py
+++ build.py
@@ -1,10 +1,8 @@
 import os
 import re
 
-
 # -- Edit this first --
 PATH_SETENV = r'C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.Cmd'
-
 
 def main():
 	# Get new name
@@ -12,7 +10,7 @@ def main():
 		txt = f.read()
 	name = re.findall(r'\s+NAME\[\]\s*=\s*_T\("(.*?)"', txt)
 	version = re.findall(r'\s+VERSION\[\]\s*=\s*_T\("(.*?)"', txt)
-	fnout = '%s-%s.exe' % (name[0], version[0])
+	fnout = r'distribution\%s-%s.exe' % (name[0], version[0])
 
 	# Build JumplistZ.exe
 	cmds = [
@@ -20,10 +18,9 @@ def main():
 		'rc JumplistZ.rc',
 		'cl JumplistZ.cpp',
 		'link JumplistZ.obj JumplistZ.res /out:%s' % fnout,
-		]
+	]
 	ret = os.system(' && '.join(cmds))
 	print 'ret: %d' % ret
-
 
 if __name__ == '__main__':
 	main()
